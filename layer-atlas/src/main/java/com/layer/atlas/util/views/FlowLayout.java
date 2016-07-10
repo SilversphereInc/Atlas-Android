4e0cb7bb0d9004699af4adb26a01ed6b1a7425dc
Justin Gagnon <jgagnontech@gmail.com>
2016-07-10 01:17:33 -0400

    v0.2.13

--- layer-atlas/src/main/java/com/layer/atlas/util/views/FlowLayout.java
+++ layer-atlas/src/main/java/com/layer/atlas/util/views/FlowLayout.java
@@ -1,7 +1,7 @@
 /**
  * Based on: https://github.com/blazsolar/FlowLayout
  *
- * Copyright 2013 Blaž Šolar
+ *
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
